5ecab0d4bd36b0e2e1cd66f26a46ad1103593adc
xutianyi <853122726@qq.com>
2021-01-05 13:53:49 +0800

    update

--- c/httptosocks.h
+++ c/httptosocks.h
@@ -5,11 +5,13 @@
 #ifndef HTTPTOSOCKS_HTTPTOSOCKS_H
 #define HTTPTOSOCKS_HTTPTOSOCKS_H
 
-extern "C" _declspec(dllexport)
+extern "C" {
 
 void start(const char *proxy_addr,
            unsigned char proxy_addr_len,
            const char *socks5_addr,
            unsigned char socks5_addr_len);
+};
+
 
 #endif //HTTPTOSOCKS_HTTPTOSOCKS_H
